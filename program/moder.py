c221f558b06771d5d8326085642e184d12e0df78
soFiyaBRS99 <165374490+soFiyaBRS99@users.noreply.github.com>
2024-04-01 12:17:36 +0300

    Create moder.py

---
+++ program/moder.py
@@ -0,0 +1,12 @@
+class Moderator:
+  def __init__(self):
+    #Инициализация класса модератроа
+
+  def ChangeComment(self):
+    #Модуль управления комментариями (редактирование, удаление)
+
+  def BanUser(self):
+    #Модуль блокировки пользователей
+
+  def ChangeTagsAndCom(self):
+    #Модуль переноса тем и комментариев
