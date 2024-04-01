f4dd16216ceee6d22b9a8edb48553310358677a2
soFiyaBRS99 <165374490+soFiyaBRS99@users.noreply.github.com>
2024-04-01 12:10:50 +0300

    Update tags.py

--- program/tags.py
+++ program/tags.py
@@ -1,1 +1,18 @@
+class Tags:
+  def __init__(self):
+    #
 
+  def CreateTags(self):
+    #Модуль создания темы
+
+  def ShowTags(self):
+    #Модуль отображения списка тем
+
+  def ShowNeedTags(self, tag):
+    #Модуль отображения конкретной темы
+
+  def CommentTags(self):
+    #Модуль комментирования темы
+
+  def SearchTags(self):
+    #Модуль поиска по темам
