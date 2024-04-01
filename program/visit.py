cd18f635540700529e5b73e4a49bca583a52c529
soFiyaBRS99 <165374490+soFiyaBRS99@users.noreply.github.com>
2024-04-01 12:16:25 +0300

    Create visit.py

---
+++ program/visit.py
@@ -0,0 +1,12 @@
+class Visitor:
+  def __init__(self):
+    #Инициализация класса посетителя
+
+  def AddComment(self):
+    #Модуль добавления комментариев к темам
+
+  def LikeOrDislike(self):
+    #Модуль лайков и дизлайков комментариев
+
+  def CreateMes(self):
+    #Модуль создания личных сообщений
