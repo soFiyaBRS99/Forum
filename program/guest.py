cea0df65263749ce5a7cb5f6ada87f7ac91bc9a1
soFiyaBRS99 <165374490+soFiyaBRS99@users.noreply.github.com>
2024-04-01 12:11:52 +0300

    Create guest.py

---
+++ program/guest.py
@@ -0,0 +1,9 @@
+class Guest(self):
+  def __init__(self):
+    #Иницилазиция класса гостя
+
+  def LookTags(self):
+    #Модуль просмотра списка тем
+
+  def LookInTags(self):
+    #Модуль просмотра содержимого темы
